6879e83a8a50aa77fe5c2f69de06674d4f82f462
Kevin <kevin.tyther@vtsd.com>
2022-10-12 09:41:35 -0400

    sadasdasd

--- Lab04bvst.java
+++ Lab04bvst.java
@@ -27,7 +27,16 @@ public class Lab04bvst extends Applet
         g.drawLine(400,150,400,225);
         // DRAW SPHERE
         g.drawOval(600,125,150,150);
-
+        g.drawOval(650,125,50,150);
+        g.drawOval(663,125,25,150);
+        g.drawOval(637,125,75,150);
+        g.drawOval(625,125,100,150);
+        g.drawOval(613,125,125,150);
+        g.drawOval(600,187,150,25);
+        g.drawOval(600,175,150,50);
+        g.drawOval(600,160,150,75);
+        g.drawOval(600,148,150,100);
+        g.drawOval(600,136,150,125);
 
         // DRAW INSCRIBED/CIRCUMSCRIBED TRIANGLE
 
@@ -40,8 +49,19 @@ public class Lab04bvst extends Applet
         g.fillRect(150,425,50,25);
         // A ^
         g.fillRect(250,425,25,125);
-        g.
-
+        g.fillRect(250,425,75,25);
+        g.fillRect(300,425,25,75);
+        g.fillRect(250,475,50,25);
+        // P ^
+        g.fillRect(350,425,25,125);
+        g.fillRect(350,425,75,25);
+        g.fillRect(350,525,75,25);
+        // C ^
+        g.fillRect(450,425,75,25);
+        g.fillRect(450,425,25,75);
+        g.fillRect(450,475,75,25);
+        g.fillRect(500,475,25,75);
+        g.fillRect(500,525,75,25);
         // DRAW PACMEN FLOWER
 
 
